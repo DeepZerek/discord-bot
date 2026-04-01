3fc91d077dcf2753028637a9242716c2cf6c1a59
TheEme1 <emanuel5646@gmail.com>
2026-04-01 00:38:06 -0300

    Mod

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def on_message(message):
                 )
 
                 await message.channel.send(
-                    f"{message.author.mention} WUAJAJA AWEONAOOOOOOO CAISTE",
+                    f"{message.author.mention} :jeje: ",
                     delete_after=4
                 )
 
